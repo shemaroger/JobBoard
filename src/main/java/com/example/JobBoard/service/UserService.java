90027bcd731b9f714a0b50912fab17a7db06b747
shemaroger <shemaroger60@gmail.com>
2024-12-13 22:12:01 +0200

    update

--- src/main/java/com/example/JobBoard/service/UserService.java
+++ src/main/java/com/example/JobBoard/service/UserService.java
@@ -44,5 +44,11 @@ public class UserService {
     public void deleteUser(User user) {
         userRepository.delete(user);
     }
-
+    public Optional<User> login(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            return user;
+        }
+        return Optional.empty();
+    }
 }
